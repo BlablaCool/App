27df4ca294703092a95a4a1573e027c959d030e9
Valentin Polo <me@valentinpolo.fr>
2015-03-23 06:50:43 +0100

    Nettoyage...

--- src/main/java/info/fges/blablacool/configuration/AppConfig.java
+++ src/main/java/info/fges/blablacool/configuration/AppConfig.java
@@ -31,43 +31,43 @@ import java.util.Properties;
 @Import({ SecurityConfig.class })
 public class AppConfig extends WebMvcConfigurerAdapter
 {
-    @Bean
-    public SessionFactory sessionFactory()
-    {
-        LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource());
-        builder.scanPackages("info.fges.blablacool.models")
-                .addProperties(getHibernateProperties());
-
-        return builder.buildSessionFactory();
-    }
-
-    private Properties getHibernateProperties()
-    {
-        Properties prop = new Properties();
-        prop.put("hibernate.format_sql", "true");
-        prop.put("hibernate.show_sql", "true");
-        prop.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-
-        return prop;
-    }
-
-    @Bean(name = "dataSource")
-    public BasicDataSource dataSource()
-    {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/blablacool");
-        ds.setUsername("blablacool");
-        ds.setPassword("blablacool");
-
-        return ds;
-    }
-
-    @Bean
-    public HibernateTransactionManager txManager()
-    {
-        return new HibernateTransactionManager(sessionFactory());
-    }
+//    @Bean
+//    public SessionFactory sessionFactory()
+//    {
+//        LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource());
+//        builder.scanPackages("info.fges.blablacool.models")
+//                .addProperties(getHibernateProperties());
+//
+//        return builder.buildSessionFactory();
+//    }
+//
+//    private Properties getHibernateProperties()
+//    {
+//        Properties prop = new Properties();
+//        prop.put("hibernate.format_sql", "true");
+//        prop.put("hibernate.show_sql", "true");
+//        prop.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+//
+//        return prop;
+//    }
+//
+//    @Bean(name = "dataSource")
+//    public BasicDataSource dataSource()
+//    {
+//        BasicDataSource ds = new BasicDataSource();
+//        ds.setDriverClassName("com.mysql.jdbc.Driver");
+//        ds.setUrl("jdbc:mysql://localhost:3306/blablacool");
+//        ds.setUsername("blablacool");
+//        ds.setPassword("blablacool");
+//
+//        return ds;
+//    }
+//
+//    @Bean
+//    public HibernateTransactionManager txManager()
+//    {
+//        return new HibernateTransactionManager(sessionFactory());
+//    }
 
     @Bean
     public InternalResourceViewResolver getInternalResourceViewResolver()
--- src/main/java/info/fges/blablacool/controllers/PageController.java
+++ src/main/java/info/fges/blablacool/controllers/PageController.java
@@ -15,40 +15,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
-public class HelloController
+public class PageController
 {
 	@Autowired
     private UserService uS;
 
     @RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model)
+	public String printWelcome()
     {
-        String password = "polopolo";
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String hashedPassword = passwordEncoder.encode(password);
-        System.out.println(hashedPassword);
-
-        model.addAttribute("message", hashedPassword);
 		return "home";
 	}
-
-    @RequestMapping({"test", "test1", "test2"})
-    @ResponseBody
-    public String getTest()
-    {
-        return "<h2>Test</h2>";
-    }
-
-    @RequestMapping({"login"})
-    public String getLogin()
-    {
-        return "auth/login";
-    }
-
-    @RequestMapping({"logout"})
-    @ResponseBody
-    public String getLogout()
-    {
-        return "<h2>Logout</h2>";
-    }
 }
