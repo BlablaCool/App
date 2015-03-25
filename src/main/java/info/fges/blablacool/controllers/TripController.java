c169c2ecb1ac5291156ff1d7dcc011171d4131fb
Valentin Polo <me@valentinpolo.fr>
2015-03-25 01:02:54 +0100

    [RESPECT DES NORMES DE CODAGE]
    Dossier /views/ :
    Respecte l'architecture des URLS (la vue pour blablacool.com/trips/new se trouve dans /views/trips/new.jsp
    
    Les JSP:
    On utilise tiles pour le templating, s'inspirer de home.jsp pour avoir un exemple de mise ne forme (plus d'inclusion directe des JSP).

--- src/main/java/info/fges/blablacool/controllers/TripController.java
+++ src/main/java/info/fges/blablacool/controllers/TripController.java
@@ -1,6 +1,9 @@
 package info.fges.blablacool.controllers;
 
 import info.fges.blablacool.models.Place;
+import info.fges.blablacool.models.Trip;
+import info.fges.blablacool.services.TripService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 /**
  * Created by Valentin on 23/03/15.
  */
@@ -16,6 +21,9 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/trips")
 public class TripController
 {
+    @Autowired
+    private TripService tripService;
+
     @Secured("ROLE_USER")
     @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
     @ResponseBody
@@ -44,4 +52,24 @@ public class TripController
 
         return "Sumitted";
     }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public ModelAndView getSearch()
+    {
+        ModelAndView mv = new ModelAndView("trips/search");
+
+        List<Trip> trips;
+        trips = tripService.findAll();
+
+        /*for (Trip trip : trips) {
+            trip.getTripHasPlaces().get().getPlace().getPublicName()
+            /*User user = trip.getDriver();
+            Car car = trip.;
+        }*/
+
+        mv.addObject("lTrips", trips);
+
+        return mv;
+    }
+
 }
