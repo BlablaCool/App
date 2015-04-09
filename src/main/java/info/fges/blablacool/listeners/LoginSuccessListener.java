be2d8ae1f6540e90ae17d8b69a8d1f7eccf22be6
Nicolas <nducom@gmail.com>
2015-04-09 22:05:54 +0200

    fixed commented code on listener

--- src/main/java/info/fges/blablacool/listeners/LoginSuccessListener.java
+++ src/main/java/info/fges/blablacool/listeners/LoginSuccessListener.java
@@ -4,6 +4,7 @@ import info.fges.blablacool.configuration.AppConfig;
 import info.fges.blablacool.models.User;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.apache.log4j.Logger;
 
@@ -18,7 +19,7 @@ public class LoginSuccessListener implements ApplicationListener<InteractiveAuth
     @Override
     public void onApplicationEvent(InteractiveAuthenticationSuccessEvent event)
     {
-       // User user= (User) event.getAuthentication().getPrincipal();
-       // logger.warn("User : " + user.getUsername() + " logged in");
+        UserDetails user= (UserDetails) event.getAuthentication().getPrincipal();
+        logger.warn("User : " + user.getUsername() + " logged in");
     }
 }
